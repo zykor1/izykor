e48398febc70fa4a62b580a6185f6111eafed92d
zykor <enrique.wx@gmail.com>
2014-09-29 19:04:16 -0500

    lolololo

--- proveedores/models.py
+++ proveedores/models.py
@@ -5,3 +5,10 @@ class Proveedor(models.Model):
     nombre = models.CharField(max_length=30)
     descripcion = models.CharField(max_length=30)
     fecha = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Proveedor'
+        verbose_name_plural = 'Proveedores'
+
+    def __unicode__(self):
+        return "%s - %s" % ( self.nombre, self.descripcion)
